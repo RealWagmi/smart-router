bacf0227f6d7de72eac04850531a1fe7988e326d
Alex <tarkovskiy.alexandr@protonmail.com>
2024-01-09 18:20:58 +0200

    hide clog

--- src/constants/gasModel/index.ts
+++ src/constants/gasModel/index.ts
@@ -1,11 +1,11 @@
 import { Token, ChainId, arbitrumTokens, ethereumTokens, fantomTokens, kavaTokens, zkSyncTokens, optimismTokens, bscTokens, polygonTokens, avalancheTokens, metisTokens } from '@real-wagmi/sdk';
 
 export const usdGasTokensByChain = {
-  [ChainId.ETHEREUM]: [ethereumTokens.usdc, ethereumTokens.usdt],
+  [ChainId.ETHEREUM]: [ethereumTokens.usdt, ethereumTokens.usdc],
   [ChainId.OPTIMISM]: [optimismTokens.usdc, optimismTokens.usdt],
-  [ChainId.BSC]: [bscTokens.usdc, bscTokens.usdt],
+  [ChainId.BSC]: [bscTokens.usdt, bscTokens.usdc],
   [ChainId.POLYGON]: [polygonTokens.usdc, polygonTokens.usdt],
-  [ChainId.FANTOM]: [fantomTokens.dai, fantomTokens.frax, fantomTokens.usdc, fantomTokens.usdt],
+  [ChainId.FANTOM]: [fantomTokens.usdc, fantomTokens.dai, fantomTokens.frax, fantomTokens.usdt],
   [ChainId.ZKSYNC]: [zkSyncTokens.usdc, zkSyncTokens.busd],
   [ChainId.KAVA]: [kavaTokens.usdt, kavaTokens.usdc],
   [ChainId.AVALANCHE]: [avalancheTokens.usdc, avalancheTokens.usdt],
