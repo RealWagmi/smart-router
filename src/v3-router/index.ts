8508a27e2de49e453faf2d64dc123a346326fddc
Alex <tarkovskiy.alexandr@protonmail.com>
2024-10-24 16:11:59 +0300

    update export

--- src/v3-router/index.ts
+++ src/v3-router/index.ts
@@ -1,3 +1,4 @@
 export * as SmartRouter from './smartRouter'
 export { SwapRouter } from './utils/swapRouter'
 export { Transformer } from './utils'
+export type { SwapOptions } from './utils/swapRouter'
